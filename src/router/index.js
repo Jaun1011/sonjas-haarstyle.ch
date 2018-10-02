d69b8afb8440a421a567062bede94be61455c1c6
Jan(U117292) <jan.kuonen.93@gmail.com>
2018-10-02 08:15:26 +0200

    fix route

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,6 @@ import Team from '@/components/article/Team'
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
   routes: [
     {path: '/',component: Home},
     {path: '/dienstleistung',component: Dienstleistung},
