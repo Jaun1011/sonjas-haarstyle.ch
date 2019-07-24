55b4aebaa99df28ce14780405c0fff9f544cbb21
Jaun1011 <jan@jku.one>
2019-07-24 11:28:06 +0200

    maps fix

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -27,7 +27,7 @@
       <h1>Kontakt</h1>
 
       <a target="_blank" rel="noopener noreferrer"
-        href="https://www.google.ch/maps/place/Vechigenstrasse+16,+3076+Worb/@46.9323735,7.5625977,874m/data=!3m1!1e3!4m5!3m4!1s0x478e342edf32a533:0xce16060955cd2c49!8m2!3d46.9327893!4d7.562756">
+        href="https://www.google.ch/maps/place/Sonjas+Haarstyle+%2F+Make-up+Artist/@46.9328917,7.5611601,738m/data=!3m1!1e3!4m13!1m7!3m6!1s0x478e342edf32a533:0xce16060955cd2c49!2sVechigenstrasse+16,+3076+Worb!3b1!8m2!3d46.9327893!4d7.562756!3m4!1s0x478e342edf2eeab3:0xc35c85d84138e2eb!8m2!3d46.9327563!4d7.562753">
         Sonjas-Haarstyle<br>
         Vechigenstrasse 16<br>
         3076 Worb<br>
