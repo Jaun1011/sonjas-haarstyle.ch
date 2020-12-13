72c754a2eb4089664aa0a06c05b0e6be3f0529eb
Jan(U117292) <jan.kuonen.93@gmail.com>
2020-12-13 23:01:16 +0100

    add new message for popup

--- client/src/App.vue
+++ client/src/App.vue
@@ -13,13 +13,24 @@
 
           <h1>Meine Verhaltens-und Hygieneregeln für Ihren sicheren Coiffeurbesuch</h1>
 
-          Die Gesundheit steht an erster Stelle. Um Ihnen einen entspannten Coiffeurbesuch zu ermöglichen, habe ich anhand
+          <div class="highlightMessage">
+            Der Bundesrat hat an seiner Sitzung vom 11 Dezember 2020 seine Massnahmen gegen die Ausbreitung des
+            Coronavirus verstärkt.<br>
+
+            Das betrifft auch meine Öffnungszeiten. Dienstag & Mittwoch ist nur noch bis 19.00 Uhr geöffnet.
+            Voraussichtliche bis 22. Januar. 2021.<br>
+
+          </div>
+
+          Die Gesundheit steht an erster Stelle. Um Ihnen einen entspannten Coiffeurbesuch zu ermöglichen, habe ich
+          anhand
           der Vorgaben des BAG mein eigenes Schutzkonzept erarbeitet.<br>
           Bitte lesen Sie alles aufmerksam durch:
 
 
           <ul>
-            <li>Aus Hygienischen Gründen sind Mundschutz, Hände gründlich waschen und desinfizieren obligatorisch. Sie erhalten alles im Salon zu einem solidarischen Preis von CHF 2.00.
+            <li>Aus Hygienischen Gründen sind Mundschutz, Hände gründlich waschen und desinfizieren obligatorisch. Sie
+              erhalten alles im Salon zu einem solidarischen Preis von CHF 2.00.
             </li>
             <li>Klingeln Sie beim Eingang und warten vor der Tür bis ich Sie abhole.</li>
             <li>Wir müssen auf das gewohnte Händeschütteln verzichten.</li>
@@ -37,7 +48,8 @@
             <li>Termine können nur telefonisch vereinbart werden damit die Anzahl der Personen im Salon laut BAG nicht
               überschritten wird.
             </li>
-            <li>Es sind keine Begleitpersonen, auch keine Kinder, erlaubt. Sollte Ihr Kind ein Coiffeurbesuch haben, dann
+            <li>Es sind keine Begleitpersonen, auch keine Kinder, erlaubt. Sollte Ihr Kind ein Coiffeurbesuch haben,
+              dann
               müssen alle Beteiligten eine Schutzmaske
               tragen und meine Hygieneregeln befolgen. Nach Möglichkeit warten die Eltern draussen.
             </li>
@@ -72,155 +84,159 @@
 </template>
 
 <script>
-  import Navigation from './components/Navigation.vue';
-  import Footer from './components/Footer.vue';
+import Navigation from './components/Navigation.vue';
+import Footer from './components/Footer.vue';
 
-  export default {
-    name: 'App',
-    components: {
-      Footer,
-      Navigation
-    },
-    data() {
-      return {
-        popup: true
-      };
-    },
-    methods: {
-      route: function (route) {
-        this.$router.push(route)
-      }
-
+export default {
+  name: 'App',
+  components: {
+    Footer,
+    Navigation
+  },
+  data() {
+    return {
+      popup: true
+    };
+  },
+  methods: {
+    route: function (route) {
+      this.$router.push(route)
     }
+
   }
+}
 </script>
 
 <style>
-  .popup {
+.popup {
 
-    background-color: #7777779e;
-    position: absolute;
-    width: 100%;
-    z-index: 999;
-    color: black;
-  }
+  background-color: #7777779e;
+  position: absolute;
+  width: 100%;
+  z-index: 999;
+  color: black;
+}
 
-  .popup-button {
-    margin-left: 100%;
+.popup-button {
+  margin-left: 100%;
 
-  }
+}
+
+.highlightMessage{
+  color: red;
+  padding-bottom: 10px;
+}
+
+.popup-text {
+  margin: 15%;
 
 
-  .popup-text {
-    margin: 15%;
+  padding: 20px;
+  background-color: whitesmoke;
+}
 
+.popup a {
+  color: #e80bc5;
+  border: solid 1px #e80bc5;
 
-    padding: 20px;
-    background-color: whitesmoke;
-  }
+}
 
-  .popup a {
-    color: #e80bc5;
-    border: solid 1px #e80bc5;
+table, tr, td, th {
+  margin: 0;
+  padding: 0;
+}
 
-  }
+h1 {
+  font-size: 24px;
+}
 
-  table, tr, td, th {
-    margin: 0;
-    padding: 0;
-  }
+h2 {
+  font-size: 22px;
+}
 
-  h1 {
-    font-size: 24px;
-  }
+h3 {
+  font-size: 20px;
+}
 
-  h2 {
-    font-size: 22px;
-  }
+h1, h2, h3 {
+  margin: 10px 0 10px;
+}
 
-  h3 {
-    font-size: 20px;
-  }
+html, body, #app {
+  font-size: 16px;
+  height: 100%;
+  width: 100%;
+}
 
-  h1, h2, h3 {
-    margin: 10px 0 10px;
-  }
+body {
+  background-color: #030102;
+  margin: 0;
+  overflow-y: scroll;
+}
 
-  html, body, #app {
-    font-size: 16px;
-    height: 100%;
-    width: 100%;
-  }
+article {
+  font-size: 1em;
+  background-color: #1b1b1b;
+}
 
-  body {
-    background-color: #030102;
-    margin: 0;
-    overflow-y: scroll;
-  }
+.text {
+  background-color: #10110d;
+  padding: 1em;
+  border: solid 3px black;
+  margin: 1em;
+}
 
-  article {
-    font-size: 1em;
-    background-color: #1b1b1b;
-  }
+.image {
+  display: block;
+  padding: 1em 2em;
+  margin: 1em;
+}
 
-  .text {
-    background-color: #10110d;
-    padding: 1em;
-    border: solid 3px black;
-    margin: 1em;
-  }
+.roundImage {
+  border: 2px solid #e80bc5;
+  border-radius: 50%;
+  height: 250px;
+  width: 250px;
+  background-color: whitesmoke;
+}
 
-  .image {
-    display: block;
-    padding: 1em 2em;
-    margin: 1em;
-  }
+#app {
+  grid-template-columns: 10% auto 10%;
+  grid-template-rows: auto auto 1fr auto;
 
-  .roundImage {
-    border: 2px solid #e80bc5;
-    border-radius: 50%;
-    height: 250px;
-    width: 250px;
-    background-color: whitesmoke;
-  }
+  display: grid;
+  background-color: rgba(3, 1, 2, 0.78);
+  color: #f7fcff;
+}
 
-  #app {
-    grid-template-columns: 10% auto 10%;
-    grid-template-rows: auto auto 1fr auto;
+header, main, article, footer {
+  grid-column: 2 /3;
+}
 
-    display: grid;
-    background-color: rgba(3, 1, 2, 0.78);
-    color: #f7fcff;
-  }
+header img {
+  display: block;
+  width: 70%;
 
-  header, main, article, footer {
-    grid-column: 2 /3;
-  }
+  margin: 0px auto;
+  max-height: 500px;
 
-  header img {
-    display: block;
-    width: 70%;
+}
 
-    margin: 0px auto;
-    max-height: 500px;
+/*Scrollbar*/
+::-webkit-scrollbar {
+  width: 10px;
+}
 
-  }
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
 
-  /*Scrollbar*/
-  ::-webkit-scrollbar {
-    width: 10px;
-  }
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
 
-  ::-webkit-scrollbar-track {
-    background: #f1f1f1;
-  }
-
-  ::-webkit-scrollbar-thumb {
-    background: #888;
-  }
-
-  ::-webkit-scrollbar-thumb:hover {
-    background: #555;
-  }
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
 
 </style>
